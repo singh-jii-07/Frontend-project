bc557da5f757ef2bb31065b7211a37695636196e
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-04-14 12:00:21 -0500

    new file add

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,8 +1,33 @@
 import MemberRoute from "./member";
 import AdminRoute from "./admin";
 import { createBrowserRouter } from "react-router-dom";
+import { lazy } from "react";
+import LandingLayout from "../Layouts/Landing";
+
+const Unauthorized=lazy (()=> import("../Views/error/Unauthorized"));
+const LandingPage = lazy(()=>import('../Views/Landing'))
+const LandingRoute={
+    path:"/",
+    element:<LandingLayout/>,
+    children: [
+        {
+            path:"/",
+            element:<LandingPage/>
+        },
+        {
+            path:"/401",element:<Unauthorized/>
+        },{
+            path:"*",
+            element:
+            <div className="h-screen flex justify-center items-center text-9xl font-bold text-yellow-500">
+                404
+            </div>
+        }
+    ]
+}
 const route= createBrowserRouter([
     AdminRoute,
-    MemberRoute
+    MemberRoute,
+    LandingRoute
 ])
 export default route;
---
+++ src/Views/error/Unauthorized.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Unauthorized = () => {
+  return (
+    <div>
+      401
+    </div>
+  )
+}
+
+export default Unauthorized
